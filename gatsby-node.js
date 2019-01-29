49373080b4221d846d8054d7a833a91c2fcd0fc4
Joerg Schneider <joerg.schneider@z-lab.com>
2019-01-29 11:34:06 +0100

    added modernizr feature-detection to webpack config in gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,30 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+
+/**
+ * Modernizr feature detects for older browsers
+ */
+exports.onCreateWebpackConfig = ({ stage, plugins, actions }) => {
+    var ModernizrWebpackPlugin = require('modernizr-webpack-plugin');
+
+    var config = {
+        'options':[
+            'setClasses'
+        ],
+        //minify: true,
+        'feature-detects': [
+          'input',
+          'canvas',
+          'css/resize',
+          'css/wrapflow'
+        ]
+      }
+       
+
+    actions.setWebpackConfig({
+        plugins:[
+            new ModernizrWebpackPlugin(config)
+        ]
+    })
+}
