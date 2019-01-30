cb3609fccbd8c4c66b78265e08f1ca5e2558a6a2
Sebastian Bartus-Kunz <sebastian.bartus-kunz@z-lab.com>
2019-01-30 22:43:50 +0100

    Finished CD to S3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,3 @@
-const targetAddress = new URL(process.env.TARGET_ADDRESS || `http://design-system-website.test/`);
 module.exports = {
   siteMetadata: {
     title: `Zeppelin Design System`,
@@ -28,22 +27,9 @@ module.exports = {
         icon: `src/images/zeppelin_icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sass`,
+    `gatsby-plugin-sass`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-    {
-      resolve: `gatsby-plugin-s3`,
-      options: {
-          bucketName: process.env.TARGET_BUCKET_NAME || "fake-bucket",
-          region: process.env.AWS_REGION,
-          protocol: targetAddress.protocol.slice(0, -1),
-          hostname: targetAddress.hostname,
-          acl: null,
-          params: {
-              // In case you want to add any custom content types: https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/custom-content-type.md
-          },
-      },
-  }
   ],
 }
