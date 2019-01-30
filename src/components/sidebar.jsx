304718b570b6c0aedc7e3482c6c4312c824194bf
Alyssa Butler <alyssabutler@Alyssas-MacBook-Pro.local>
2019-01-30 12:01:33 +0100

    sidebar layout completed

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+//import PropTypes from 'prop-types'
+
+const Sidebar = () => (
+  <div className="zds-sidebar">
+    <div className="zds-header">
+      <div className="zds-header--container">logo</div>
+    </div>
+    <div className="zds-sidebar--container" />
+  </div>
+)
+
+Sidebar.propTypes = {}
+
+Sidebar.defaultProps = {}
+
+export default Sidebar
