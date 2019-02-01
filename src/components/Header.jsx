78d9e767b791f6dde08c1391e03f8d8bf37efe6c
Alyssa ZLab <alyssa.butler.extern@z-lab.com>
2019-02-01 11:20:06 +0100

    header icons added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-import zeppelinIcon from '../assets/images/zeppelin_icon.png'
+import ZeppelinIcon from './icons/ZeppelinIcon'
+import MenuIcon from './icons/MenuIcon'
+import SearchIcon from './icons/SearchIcon'
 
 const Header = ({ siteTitle }) => (
   <header>
     <div className="zds-header">
-      <div className="zds-header--container zds-header--container__logo">
-        <Link to="/">
-          <img src={zeppelinIcon} alt="Zeppelin Icon" />
-        </Link>
-      </div>
       <div className="zds-header--container">
-        <p className="zds-header--headline">{siteTitle}</p>
+        <MenuIcon className="zds-footer--img" />
+        <ZeppelinIcon className="zds-footer--img" />
+        <SearchIcon className="zds-footer--img" />
       </div>
     </div>
   </header>
