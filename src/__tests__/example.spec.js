991c9d01b6586dfa54aeb133bc2fb353d5d38027
Sebastian Bartus-Kunz <sebastian.bartus-kunz@z-lab.com>
2019-02-05 23:36:54 +0100

    Installed eslint and recommended configuration

--- src/__tests__/example.spec.js
+++ src/__tests__/example.spec.js
@@ -1,9 +1,9 @@
-//import React from "react"
-//import renderer from "react-test-renderer"
+// import React from "react"
+// import renderer from "react-test-renderer"
 
-describe("Test example", () => {
-  it("works correctly", () => {
-    let _true = true;
-    expect(_true).toBeTruthy();
+describe('Test example', () => {
+  it('works correctly', () => {
+    let _true = true
+    expect(_true).toBeTruthy()
   })
 })
