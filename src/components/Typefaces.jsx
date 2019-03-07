3b4b73b0b9c127ae560c2b5dcb9d6f50806de1ba
Alyssa <alyssa.butler.extern@z-lab.com>
2019-03-07 15:50:40 +0100

    table completed

--- src/components/Typefaces.jsx
+++ src/components/Typefaces.jsx
@@ -9,33 +9,65 @@ const styles = theme => ({
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
-    marginBottom: theme.spacing.xl,
-    background: theme.colors.gray.grayLight.hex,
+    marginBottom: theme.spacing.xl * 2,
   },
   container: {
     flexBasis: '48%',
   },
-  teko: {
-    fontFamily: 'Teko',
+  titleContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 136,
+    background: theme.colors.gray.grayLighter.hex,
   },
-  roboto: {
-    fontFamily: 'Roboto',
+  title: {
+    fontSize: 28,
   },
   buttonContainer: {
     display: 'flex',
+    alignItems: 'center',
     justifyContent: 'space-between',
-    background: theme.colors.gray.grayLighter.hex,
+    background: theme.colors.gray.grayLightest.hex,
+    paddingTop: theme.spacing.m,
+    paddingBottom: theme.spacing.m,
+    paddingLeft: theme.spacing.l,
+    paddingRight: theme.spacing.l,
+    borderTop: `1px solid ${ theme.colors.gray.grayLight.hex }`,
+  },
+  text: {
+    fontSize: 12,
+    textTransform: 'capitalize',
+  },
+  link: {
+    color: 'inherit',
+    '&:hover': {
+      color: theme.colors.primary.indigoBlue.hex,
+    },
   },
 })
 
 const Typefaces = ({ classes }) => (
   <div className={classes.root}>
     {Object.keys(theme.typography.typefaces).map((elem, idx) => (
-      <div className={classes.container}>
-        <p className={`classes.${ elem }`}>Lorem Ipsum</p>
+      <div className={classes.container} key={`typeface${ idx }`}>
+        <div className={classes.titleContainer}>
+          <p className={classes.title} style={{ fontFamily: elem }}>
+            Lorem Ipsum
+          </p>
+        </div>
         <div className={classes.buttonContainer}>
-          <p>{elem} Font Family</p>
-          <LaunchIcon />
+          <p className={classes.text}>{elem} Font Family</p>
+          <a
+            href={`https://fonts.google.com/specimen/${ elem.charAt(0).toUpperCase() +
+              elem.slice(1).toLowerCase() }`}
+            target="_blank"
+            rel="noopener"
+            aria-label="Launch"
+            className={classes.link}
+          >
+            <LaunchIcon width="24" height="24" />
+          </a>
         </div>
       </div>
     ))}
@@ -44,7 +76,6 @@ const Typefaces = ({ classes }) => (
 
 Typefaces.propTypes = {
   classes: PropTypes.object.isRequired,
-  section: PropTypes.string.isRequired,
 }
 
 export default withStyles(styles)(Typefaces)
