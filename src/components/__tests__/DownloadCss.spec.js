8d32606bf8139111b81bb64fe2951a01c5269a23
Alyssa <alyssa.butler.extern@z-lab.com>
2019-03-11 10:47:19 +0100

    update failing tests

--- src/components/__tests__/DownloadCss.spec.js
+++ src/components/__tests__/DownloadCss.spec.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { render } from 'react-testing-library'
+import { ThemeProvider } from 'react-jss'
+import theme from '../../data/theme'
 import DownloadCss from '../DownloadCss'
 
 describe('DownloadCss', () => {
   it('renders correctly', () => {
-    const component = render(<DownloadCss />)
+    const component = render(
+      <ThemeProvider theme={theme}>
+        <DownloadCss />
+      </ThemeProvider>
+    )
     expect(component).toMatchSnapshot()
   })
 })
