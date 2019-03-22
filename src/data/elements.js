d4bcc20eed620ee1391f8980f3d2f95e2d55e58c
Sebastian Bartus-Kunz <sebastian.bartus-kunz@z-lab.com>
2019-03-22 13:40:30 +0100

    Fixed some conflicts

--- src/data/elements.js
+++ src/data/elements.js
@@ -66,4 +66,52 @@ module.exports = {
   //     vue: false,
   //   },
   // },
+  tag: {
+    demo: {
+      primary: {
+        js: [
+          `<button type="button" class="zep-button zep-button-primary">primary</button>`,
+          `<button type="button" class="zep-button zep-button-primary" disabled>disabled</button>`,
+        ],
+        react: false,
+        angular: false,
+        vue: false,
+      },
+      secondary: {
+        js: [
+          `<button type="button" class="zep-button zep-button-secondary">secondary</button>`,
+          `<button type="button" class="zep-button zep-button-secondary" disabled>
+            disabled
+          </button>`,
+        ],
+        react: false,
+        angular: false,
+        vue: false,
+      },
+    },
+    typography: {
+      'zep-typo--special-4': {
+        px: '24px',
+        rem: '1.5rem',
+        specifications: [
+          'font-family: "Teko"',
+          'font-weight: 400 (regular)',
+          'line-height: 1.5 (24px)',
+          'text-transform: uppercase',
+        ],
+      },
+    },
+    structure: {
+      'btn-height': {
+        property: 'height',
+        rem: '3rem',
+        px: '48px',
+      },
+      'btn-padding': {
+        property: 'padding',
+        rem: '1rem, 1.125rem',
+        px: '16px, 18px',
+      },
+    },
+  },
 }
