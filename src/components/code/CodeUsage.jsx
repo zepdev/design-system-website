54c62a3e9b22c37105991a4be7f6280a89daa900
Alyssa <alyssa.butler.extern@z-lab.com>
2019-03-27 12:45:50 +0100

    refactored code section

--- src/components/code/CodeUsage.jsx
+++ src/components/code/CodeUsage.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+import MDXRenderer from 'gatsby-mdx/mdx-renderer'
 import withStyles from 'react-jss'
 import classnames from 'classnames'
 import Tabs from '../tabs/Tabs'
 import Tab from '../tabs/Tab'
-import CodeBlock from './CodeBlock'
 
 const styles = theme => ({
   root: {
@@ -17,26 +18,58 @@ const styles = theme => ({
 
 const CodeUsage = ({ element, classes }) => {
   const [tab, setTab] = useState(0)
-
+  const [elem] = useState(element)
   const handleClick = (e, value) => {
     setTab(value)
   }
 
   return (
-    <div className={classes.root}>
-      <h3 className={classnames(classes.heading, 'zep-typo--normal-4')}>Code Usage Examples</h3>
-      <Tabs value={tab} onClick={handleClick} fullWidth>
-        <Tab label="React" />
-        <Tab label="Vue" />
-      </Tabs>
-      <CodeBlock>{element[tab === 0 ? 'react' : 'vue'].join('\n')}</CodeBlock>
-    </div>
+    <StaticQuery
+      query={graphql`
+        query {
+          allMdx {
+            edges {
+              node {
+                id
+                frontmatter {
+                  title
+                  label
+                }
+                code {
+                  body
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        const react = data.allMdx.edges.find(
+          x => x.node.frontmatter.label === `${ elem }CodeExample_React`
+        )
+        const vue = data.allMdx.edges.find(
+          x => x.node.frontmatter.label === `${ elem }CodeExample_Vue`
+        )
+        return (
+          <div className={classes.root}>
+            <h3 className={classnames(classes.heading, 'zep-typo--normal-4')}>
+              Code Usage Examples
+            </h3>
+            <Tabs value={tab} onClick={handleClick} fullWidth>
+              <Tab label="React" />
+              <Tab label="Vue" />
+            </Tabs>
+            <MDXRenderer>{tab === 0 ? react.node.code.body : vue.node.code.body}</MDXRenderer>
+          </div>
+        )
+      }}
+    />
   )
 }
 
 CodeUsage.propTypes = {
   classes: PropTypes.object.isRequired,
-  element: PropTypes.object.isRequired,
+  element: PropTypes.string.isRequired,
 }
 
 export default withStyles(styles)(CodeUsage)
