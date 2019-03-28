bfd4f6b59e7ec0ac67a55e1d9e7a448dc6fbd0fa
Alyssa <alyssa.butler.extern@z-lab.com>
2019-03-28 13:58:39 +0100

    remove react-live

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,7 +9,6 @@ import Header from './Header'
 import Footer from './Footer'
 import Sidebar from './sidebar/Sidebar'
 import CodeBlock from './code/CodeBlock'
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 import 'zeppelin-element-library/bundle/zeppelin-element-library.css'
 
 const styles = theme => ({
@@ -83,14 +82,6 @@ const styles = theme => ({
   },
 })
 
-const MyCodeComponent = ({ children, ...props }) => (
-  <LiveProvider code={children}>
-    <LiveEditor />
-
-    <LivePreview />
-  </LiveProvider>
-)
-
 function Layout({ children, classes }) {
   const [isMenuOpen, setMenu] = useState(false)
   // Styles for mdx/md pages
@@ -118,7 +109,7 @@ function Layout({ children, classes }) {
     p: pStyled,
     hr: hrStyled,
     pre: preStyled,
-    code: MyCodeComponent,
+    code: CodeBlock,
   }
   return (
     <StaticQuery
