73a7cdfa68ed71c700b318277da05dd0bb35f5d5
Sebastian Bartus-Kunz <sebastian.bartus-kunz@z-lab.com>
2019-04-02 13:59:33 +0200

    ZDS-312 fixed test for Layout

--- src/components/__tests__/Layout.spec.js
+++ src/components/__tests__/Layout.spec.js
@@ -3,12 +3,26 @@ import { render } from 'react-testing-library'
 import { ThemeProvider } from 'react-jss'
 import theme from '../../data/theme'
 import Layout from '../Layout'
+import { StaticQuery } from 'gatsby'
+
+beforeEach(() => {
+  StaticQuery.mockImplementationOnce(({ render }) =>
+    render({
+      mdx: {
+        frontmatter: {
+          title: `Mock Title`,
+        },
+      },
+    })
+  )
+})
 
 describe('Layout', () => {
   it('renders correctly', () => {
     const mockProps = {
-      children: '',
+      children: '<div>mock-div</div>',
     }
+
     const component = render(
       <ThemeProvider theme={theme}>
         <Layout {...mockProps} />
