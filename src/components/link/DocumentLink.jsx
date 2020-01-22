6dd9892299ef855ce681523c18667b42c0e674b0
Alyssa <alyssa.butler.extern@z-lab.com>
2020-01-22 16:34:28 +0100

    updated getting-stated docs

--- src/components/link/DocumentLink.jsx
+++ src/components/link/DocumentLink.jsx
@@ -1,29 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import withStyles from 'react-jss'
-import clsx from 'clsx'
 import { Link } from 'gatsby'
 
 const styles = theme => ({
   root: {
-    marginBottom: `${ theme.spacing.component.xxl.rem }rem`,
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  link: {
-    flexBasis: '100%',
-  },
-  [`@media (min-width: ${ theme.breakpoints.l })`]: {
-    link: {
-      flexBasis: '50%',
-    },
+    marginBottom: `${theme.spacing.component.xxl.rem}rem`,
   },
 })
 
 const DocumentLink = ({ text, to, classes }) => {
   return (
     <div className={classes.root}>
-      <Link to={to} className={clsx('zep-button zep-button-primary', classes.link)}>
+      <Link to={to} className="zep-button zep-button-primary">
         <span className="zep-button__text">{text}</span>
       </Link>
     </div>
