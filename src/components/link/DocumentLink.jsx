61221bc6495db3ba3cec88b44c5eb60e6a236a6b
Alyssa <alyssa.butler.extern@z-lab.com>
2020-03-18 10:00:49 +0100

    remove old test

--- src/components/link/DocumentLink.jsx
+++ src/components/link/DocumentLink.jsx
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    marginBottom: `${theme.space.xxl.rem}rem`,
+    marginBottom: `${ theme.space.xxl.rem }rem`,
     width: 230,
   },
 }))
