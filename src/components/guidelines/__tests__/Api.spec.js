945babe7da8ed59bb7735b4297806d7cf8b83c15
Alyssa <alyssa.butler.extern@z-lab.com>
2021-01-08 13:59:29 +0100

    addition to api test

--- src/components/guidelines/__tests__/Api.spec.js
+++ src/components/guidelines/__tests__/Api.spec.js
@@ -1,10 +1,11 @@
 import React from "react"
-import { render } from "../../../../test-utils"
+import { render, screen } from "../../../../test-utils"
+import "@testing-library/jest-dom/extend-expect"
 import Api from "../Api"
 
 describe("Api", () => {
   it("renders correctly", () => {
-    const component = render(<Api />)
-    expect(component).toMatchSnapshot()
+    render(<Api />)
+    expect(screen.getAllByRole("treeitem")).toHaveLength(7)
   })
 })
