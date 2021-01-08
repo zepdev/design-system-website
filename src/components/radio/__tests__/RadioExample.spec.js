8a82b3dbbf3acc4e74dfe5f6c0267335077f4cc0
Alyssa <alyssa.butler.extern@z-lab.com>
2021-01-08 14:23:25 +0100

    new radio tests

--- src/components/radio/__tests__/RadioExample.spec.js
+++ src/components/radio/__tests__/RadioExample.spec.js
@@ -1,10 +1,36 @@
 import React from "react"
-import { render } from "../../../../test-utils"
+import { render, fireEvent } from "../../../../test-utils"
+import "@testing-library/jest-dom/extend-expect"
 import RadioExample from "../RadioExample"
 
 describe("RadioExample", () => {
   it("renders correctly", () => {
     const component = render(<RadioExample />)
     expect(component).toMatchSnapshot()
+  })
+  it("changes to B", () => {
+    const { getAllByLabelText } = render(<RadioExample />)
+    const radio = getAllByLabelText("radioGroup")[0]
+    fireEvent.change(radio, { target: { value: "b" } })
+    expect(radio.value).toBe("b")
+  })
+  it("changes to A", () => {
+    const { getAllByLabelText } = render(<RadioExample />)
+    const radio = getAllByLabelText("radioGroup")[0]
+    fireEvent.click(radio, { target: { value: "a" } })
+    expect(radio.value).toBe("a")
+  })
+  it("changes inline to A", () => {
+    const { getAllByLabelText } = render(<RadioExample />)
+    const radio = getAllByLabelText("radioGroup2")[0]
+    fireEvent.click(radio, { target: { value: "a" } })
+    expect(radio.value).toBe("a")
+    expect(getAllByLabelText("radioGroup")[0].checked).toBe(true)
+  })
+  it("changes inline to B", () => {
+    const { getAllByLabelText } = render(<RadioExample />)
+    const radio = getAllByLabelText("radioGroup2")[0]
+    fireEvent.change(radio, { target: { value: "b" } })
+    expect(radio.value).toBe("b")
   })
 })
