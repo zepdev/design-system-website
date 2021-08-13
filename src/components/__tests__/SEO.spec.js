005471df4dae1c6ac09136598f139aa45041310a
Alyssa <alyssa.butler.extern@z-lab.com>
2021-08-13 15:55:57 +0200

    fix testing paths

--- src/components/__tests__/SEO.spec.js
+++ src/components/__tests__/SEO.spec.js
@@ -1,7 +1,7 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import { ThemeProvider } from "@material-ui/styles"
-import theme from "../../../src/gatsby-theme-material-ui-top-layout/theme"
+import theme from "../../../src/theme"
 import SEO from "../SEO"
 import { StaticQuery } from "gatsby"
 
